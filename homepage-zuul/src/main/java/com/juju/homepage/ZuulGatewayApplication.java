6a99315a35afdc79c21ef9d65210b8b2f8e04d59
zhengjushi <15706639196@163.com>
2019-07-29 20:43:15 +0800

    eureka over

---
+++ homepage-zuul/src/main/java/com/juju/homepage/ZuulGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.juju.homepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ *
+ *     <h1>网关应用程序</h1>
+ *     @EnableZuulProxy 标识当前应用是zuul
+ *     @SpringCloudApplication 用于简化配置的组合注解
+ * 
+**/
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class,args);
+    }
+}
